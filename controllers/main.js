6b168f791fc3573bafc083a9d81fa67b5ed8835e
Gonzalo <113151385+GonzaloStickar@users.noreply.github.com>
2024-03-28 17:20:45 -0300

    Fix de tiempos de carga de películas

--- controllers/main.js
+++ controllers/main.js
@@ -2,14 +2,14 @@ const axios = require('axios');
 const { request, response} = require('express');
 require('dotenv').config();
 const fs = require('fs');
+const palabrasTresCaracteres = JSON.parse(fs.readFileSync('./controllers/palabras.json'));
 
 const clave = process.env.API_KEY;
 
-function numeroRandom(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
-}
+const obtenerPalabraAleatoria = () => {
+    const randomIndex = Math.floor(Math.random() * palabrasTresCaracteres.length);
+    return palabrasTresCaracteres[randomIndex];
+};
 
 const getPeliculas = async (req, res) => {
 
@@ -21,36 +21,10 @@ const getPeliculas = async (req, res) => {
         const sinopsis_peliculas = [];
         const id_peliculas = [];
 
-        const obtenerPalabraAleatoria = async () => {
+        const obtenerInfoPelicula = async () => {
             try {
-                const response = await axios.get('https://www.palabrasque.com/palabra-aleatoria.php?Submit=Nueva+palabra');
-                if (response.status === 200) {
-                    const html = response.data;
-                    const regex = /<font data="palabra" size="6" \/><b>(\w+)<\/b><\/font>/;
-                    const match = html.match(regex);
-                    if (match) {
-                        return match[1];
-                    }
-                    else {
-                        console.log('No se encontró una palabra aleatoria en la página.');
-                        return obtenerPalabraAleatoria();
-                    }
-                }
-                else {
-                    console.log('Error al obtener la página:', response.status);
-                    return obtenerPalabraAleatoria();
-                }
-            }
-            catch (error) {
-                console.error(error);
-                throw error;
-            }
-        };
-        const obtenerInfoPelicula = async (palabra) => {
-            const palabraSinPrimerCaracterYEnMinusculas = palabra.substring(1).toLowerCase();
-            const palabraPrimerosTresCaracteres = palabraSinPrimerCaracterYEnMinusculas.substring(0, numeroRandom(1, 7));
+                const palabraPrimerosTresCaracteres = obtenerPalabraAleatoria();
 
-            try {
                 const { data } = await axios.get(`https://www.omdbapi.com/?t=${palabraPrimerosTresCaracteres}&apikey=${clave}`);
 
                 if (data.Response === "True") {
@@ -74,7 +48,7 @@ const getPeliculas = async (req, res) => {
                                 sinopsis_peliculas.push(data2.Plot);
                             }
                             else {
-                                sinopsis_peliculas.push("Error al buscar una sinopsis / No se encontró una sinopsis");
+                                sinopsis_peliculas.push("No se encontró una sinopsis");
                             }
                         }
                         catch (omdbError) {
@@ -282,10 +256,9 @@ const getPelicula = (req, res) => {
         const pelicula = `
             <main>
                 <article>
-                  
                     <div class="info_pelicula">
-                        <img src="${peli.Poster && peli.Poster !== "N/A" ? peli.Poster : 'Póster no disponible'}" alt="${peli.Title}">
                         <p>${peli.Title && peli.Title !== "N/A" ? peli.Title : 'Título no disponible'}</p>
+                        <img src="${peli.Poster && peli.Poster !== "N/A" ? peli.Poster : 'Póster no disponible'}" alt="${peli.Title}">
                         <p>${peli.Plot && peli.Plot !== "N/A" ? peli.Plot : 'Trama no disponible'}</p>
                         ${peli.Director && peli.Director !== "N/A" ? `<p><strong>Director: </strong> ${peli.Director}</p>` : ''}
                         ${peli.Actors && peli.Actors !== "N/A" ? `<p><strong>Actores: </strong> ${peli.Actors}</p>` : ''}
@@ -321,73 +294,35 @@ const wrongRequest = (req = request, res = response) => {
 }
 
 const getDirectores = async (req, res) => {
-
     const paginaPrincipal = fs.readFileSync('./public/templates/peliculas.html', 'utf8');
 
     try {
         const directores_lista = [];
         const nombre_peliculas = [];
 
-        const obtenerPalabraAleatoria = async () => {
+        const obtenerInfoPelicula = async () => {
             try {
-                const response = await axios.get('https://www.palabrasque.com/palabra-aleatoria.php?Submit=Nueva+palabra');
-                if (response.status === 200) {
-                    const html = response.data;
-                    const regex = /<font data="palabra" size="6" \/><b>(\w+)<\/b><\/font>/;
-                    const match = html.match(regex);
-                    if (match) {
-                        return match[1];
-                    }
-                    else {
-                        console.log('No se encontró una palabra aleatoria en la página.');
-                        return obtenerPalabraAleatoria();
-                    }
-                }
-                else {
-                    console.log('Error al obtener la página:', response.status);
-                    return obtenerPalabraAleatoria();
-                }
-            }
-            catch (error) {
-                console.error(error);
-                throw error;
-            }
-        };
-        const obtenerInfoPelicula = async (palabra) => {
-            const palabraSinPrimerCaracterYEnMinusculas = palabra.substring(1).toLowerCase();
-            const palabraPrimerosTresCaracteres = palabraSinPrimerCaracterYEnMinusculas.substring(0, numeroRandom(1, 7));
+                const palabra = obtenerPalabraAleatoria();
 
-            try {
-                const { data } = await axios.get(`https://www.omdbapi.com/?t=${palabraPrimerosTresCaracteres}&apikey=${clave}`);
+                const { data } = await axios.get(`https://www.omdbapi.com/?t=${palabra}&apikey=${clave}`);
 
-                if (data.Response === "True") {
-                    if ("Title" in data && "Director" in data && data.Director !== "N/A" && data.Title !== "N/A") {
-                        if (!(nombre_peliculas.includes(data.Title))) {
-                            nombre_peliculas.push(data.Title);
-                            directores_lista.push(data.Director);
-                        }
+                if (data.Response === "True" && "Director" in data && data.Director !== "N/A" && "Title" in data && data.Title !== "N/A") {
+                    if (!nombre_peliculas.includes(data.Title)) {
+                        nombre_peliculas.push(data.Title);
+                        directores_lista.push(data.Director);
                     }
-                    else {
-                        const nuevaPalabra = await obtenerPalabraAleatoria();
-                        if (nuevaPalabra) {
-                            return obtenerInfoPelicula(nuevaPalabra);
-                        }
-                    }
-                }
-                else {
-                    // Si la respuesta de OMDB es 'False' o 'Not Found', intenta con otra palabra aleatoria
-                    console.log(`La película '${palabraPrimerosTresCaracteres}' no se encontró en OMDB. Intentando con otra palabra.`);
+                } else {
                     const nuevaPalabra = await obtenerPalabraAleatoria();
                     if (nuevaPalabra) {
                         return obtenerInfoPelicula(nuevaPalabra);
                     }
                 }
-            }
-            catch (error) {
+            } catch (error) {
                 console.error(error);
                 throw error;
             }
         };
+
         const promesas = Array.from({ length: 10 }, async () => {
             const palabra = await obtenerPalabraAleatoria();
             if (palabra) {
@@ -396,6 +331,11 @@ const getDirectores = async (req, res) => {
         });
 
         await Promise.all(promesas);
+
+        //Sería equivalente usar:
+        //for (let i = 0; i < 10; i++) {
+        //    await obtenerInfoPelicula();
+        //}
 
         if (directores_lista.length > 0) {
             const directores = directores_lista.map((director, i) => `
@@ -406,7 +346,7 @@ const getDirectores = async (req, res) => {
                 </ul>
             </div>
             `).join('');
-    
+
             const directoresPeliculas = `
                 <main>
                     <div class="directoresDiv">
@@ -414,23 +354,21 @@ const getDirectores = async (req, res) => {
                     </div>
                 </main>
             `;
-    
+
             const paginaConNuevoContenido = paginaPrincipal.replace(/<main>[\s\S]*<\/main>/, `<main>${directoresPeliculas}</main>`);
             res.status(200).send(paginaConNuevoContenido);
-        }
-        else {
+        } else {
             const mensajePaginaNotFound = `
                 <div class="cajaPaginaNotFound">
                     <h1>No se encontraron Directores :(</h1>
                 </div>
             `;
-    
+
             const paginaSinHeaderYFooter = paginaPrincipal
-            .replace(/<main>[\s\S]*<\/main>/, `<main>${mensajePaginaNotFound}</main>`);
+                .replace(/<main>[\s\S]*<\/main>/, `<main>${mensajePaginaNotFound}</main>`);
             res.status(200).send(paginaSinHeaderYFooter);
         }
-    }
-    catch (error) {
+    } catch (error) {
         console.error(error);
         res.status(500).json({
             status: 500,
@@ -439,8 +377,7 @@ const getDirectores = async (req, res) => {
     }
 }
 
-const getPeliculasGenero = async (req,res) => {
-
+const getPeliculasGenero = async (req, res) => {
     const { genero } = req.params;
     const generoBuscado = genero.toLowerCase();
     let generoBuscadoEnglish = "";
@@ -462,50 +399,24 @@ const getPeliculasGenero = async (req,res) => {
             generoBuscadoEnglish = "Animation";
             break;
     }
-    
+
     const paginaPrincipal = fs.readFileSync('./public/templates/peliculas.html', 'utf8');
 
     try {
         const generos_lista = [];
         const nombre_peliculas = [];
 
-        const obtenerPalabraAleatoria = async () => {
-            try {
-                const response = await axios.get('https://www.palabrasque.com/palabra-aleatoria.php?Submit=Nueva+palabra');
-                if (response.status === 200) {
-                    const html = response.data;
-                    const regex = /<font data="palabra" size="6" \/><b>(\w+)<\/b><\/font>/;
-                    const match = html.match(regex);
-                    if (match) {
-                        return match[1];
-                    }
-                    else {
-                        console.log('No se encontró una palabra aleatoria en la página.');
-                        return obtenerPalabraAleatoria();
-                    }
-                }
-                else {
-                    console.log('Error al obtener la página:', response.status);
-                    return obtenerPalabraAleatoria();
-                }
-            }
-            catch (error) {
-                console.error(error);
-                throw error;
-            }
-        };
         const obtenerInfoPelicula = async (palabra) => {
-            const palabraSinPrimerCaracterYEnMinusculas = palabra.substring(1).toLowerCase();
-            const palabraPrimerosTresCaracteres = palabraSinPrimerCaracterYEnMinusculas.substring(0, numeroRandom(1, 7));
-
             try {
+                const palabraPrimerosTresCaracteres = obtenerPalabraAleatoria();
+
                 const { data } = await axios.get(`https://www.omdbapi.com/?t=${palabraPrimerosTresCaracteres}&apikey=${clave}`);
 
                 if (data.Response === "True") {
                     if ("Title" in data && "Genre" in data && data.Genre !== "N/A" && data.Title !== "N/A") {
                         const generos = data.Genre.toLowerCase().split(', ');
 
-                        if (generos.includes(generoBuscadoEnglish.toLocaleLowerCase())) {
+                        if (generos.includes(generoBuscadoEnglish.toLowerCase())) {
                             if (!(nombre_peliculas.includes(data.Title))) {
                                 nombre_peliculas.push(data.Title);
                                 generos_lista.push(data.Genre);
@@ -533,6 +444,7 @@ const getPeliculasGenero = async (req,res) => {
                 throw error;
             }
         };
+
         const promesas = Array.from({ length: 10 }, async () => {
             const palabra = await obtenerPalabraAleatoria();
             if (palabra) {
@@ -551,7 +463,7 @@ const getPeliculasGenero = async (req,res) => {
                 </ul>
             </div>
             `).join('');
-    
+
             const generosPeliculas = `
                 <main>
                     <div class="directoresDiv">
@@ -559,7 +471,7 @@ const getPeliculasGenero = async (req,res) => {
                     </div>
                 </main>
             `;
-    
+
             const paginaConNuevoContenido = paginaPrincipal.replace(/<main>[\s\S]*<\/main>/, `<main>${generosPeliculas}</main>`);
             res.status(200).send(paginaConNuevoContenido);
         }
@@ -569,9 +481,9 @@ const getPeliculasGenero = async (req,res) => {
                     <h1>No se encontraron Películas con ese género :(</h1>
                 </div>
             `;
-    
+
             const paginaSinHeaderYFooter = paginaPrincipal
-            .replace(/<main>[\s\S]*<\/main>/, `<main>${mensajePaginaNotFound}</main>`);
+                .replace(/<main>[\s\S]*<\/main>/, `<main>${mensajePaginaNotFound}</main>`);
             res.status(200).send(paginaSinHeaderYFooter);
         }
     }
