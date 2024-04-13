40da043664a8f5180128ea4301aadd8396b2b525
Gonzalo <113151385+GonzaloStickar@users.noreply.github.com>
2024-04-13 13:16:07 -0300

    update fix

--- routes/userAgentMiddleware.js
+++ routes/userAgentMiddleware.js
@@ -1,7 +1,9 @@
 const checkUserAgent = (primeraFuncion, segundaFuncion) => {
     return (req, res) => {
         const userAgent = req.headers['user-agent'];
-        if (userAgent && (userAgent.includes('AppleWebKit') || userAgent.includes('Safari') || userAgent.includes('Chrome') || userAgent.includes('Mozilla') || userAgent.includes('Edge'))) {
+        const fromFlutterApp = req.headers['user-agent'] === 'X-Flutter-App'; // Agregar esta línea para verificar si la solicitud proviene de tu aplicación Flutter
+
+        if (fromFlutterApp || (userAgent && (userAgent.includes('AppleWebKit') || userAgent.includes('Safari') || userAgent.includes('Chrome') || userAgent.includes('Mozilla') || userAgent.includes('Edge')))) {
             primeraFuncion(req, res);
         }
         else {
